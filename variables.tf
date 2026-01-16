df78436dff70a83a2a27dbe4ce12cb65138311e9
Lovejeets <65659796+Lovejeets@users.noreply.github.com>
2026-01-16 17:08:16 +0530

    Add variables for feature toggles in Terraform

---
+++ variables.tf
@@ -0,0 +1,62 @@
+variable "enable_virtual_networks" {
+  type    = bool
+  default = false
+}
+variable "enable_nsg" {
+  description = "Enable creation of Network Security Groups"
+  type        = bool
+  default     = false
+}
+variable "enable_kv" {
+  type    = bool
+  default = false
+}
+variable "enable_log_analytics_workspace" {
+  type    = bool
+  default = false
+}
+variable "enable_storage_account" {
+  type    = bool
+  default = false
+}
+variable "enable_function_app" {
+  type    = bool
+  default = false
+}
+
+variable "enable_app_service_plan" {
+  type    = bool
+  default = false
+}
+variable "enable_user_assigned_identities" {
+  type    = bool
+  default = false
+}
+variable "enable_application_insights" {
+  type    = bool
+  default = false
+}
+variable "enable_aml_workspace" {
+  type    = bool
+  default = false
+}
+variable "enable_cognitiveservices" {
+  type    = bool
+  default = false
+}
+variable "enable_cosmosdb_account" {
+  type    = bool
+  default = false
+}
+variable "enable_role_assignments" {
+  type = bool
+  default = false
+}
+variable "enable_private_dns_zone" {
+  type = bool
+  default = false
+}
+variable "enable_private_endpoints" {
+  type = bool
+  default = false
+}
